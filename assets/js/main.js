6102f7f4a0a98fb16f56392a88ee94b1a71518a2
SimoneLollini <lollinisimone@gmail.com>
2022-10-11 14:03:32 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+//mail
+//Chiedi all’utente la sua email
+//prompt
+//controlla che sia nella lista di chi può accedere
+//verifico con if statement se l'email è presente nella mia lista 
+//array con email partecipanti
+//stampa un messaggio appropriato sull’esito del controllo.
+//insertAdjacentHTML
+
+
+//Gioco dei dadi
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//Math.floor(Math.random() * 10) + 1;
+//chi fa il punteggio più alto vince.
+//decretare il numero più alto con if statement
